ec20085b2998bdc89111f9064863154cfbbc6f37
youssef el faquir <youssef1988islam@gmail.com>
2020-07-11 20:52:08 +0100

    final

--- ie.js
+++
@@ -1,7 +0,0 @@
-if (window.document.documentMode) {
-    if (document.readyState === 'loading') {
-        document.addEventListener('DOMContentLoaded', Animate()); // Document still loading so DomContentLoaded can still fire :)
-    } else {
-        console.log("hi");
-    }
-}
